c88a6f08119d35fdaec400bd1ff6496dfe52f5e0
Vinay <vinayiet435@gmail.com>
2026-09-11 11:06:38 +0530

    added new line from vscode

--- new.py
+++ new.py
@@ -1,1 +1,12 @@
 print('This is the new line over here.')
+print('this is new line from the vscode.')
+# First create the repository on the Github 
+# Clone that repository into the system to work on that.
+# if you have made some changes into the repository 
+# then you've to follow these steps 
+# 1. git add file_name
+# 2. git commit -m "any message"
+# 3. git push 
+
+# Every time you make any changes into the project then you 
+# must have to follow these above steps.
